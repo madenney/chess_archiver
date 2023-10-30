02ec79d2c045e773a488718d293308459227bc6f
Matt Denney <matt.a.denney@gmail.com>
2023-10-29 22:40:54 -0700

    huh

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import os
+import io
 import sys
 import time
 import signal
@@ -13,12 +14,13 @@ import pyautogui
 import numpy as np
 import common as c
 
-from pgn_parser import parser, pgn
 
 import chess
 import chess.pgn
 
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -26,26 +28,30 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 from PIL import Image, ImageOps, ImageDraw, ImageFont
 
-
-TIME_PER_MOVE = 0.08
-MIN_MOVES = 70
-PRE_GAME_PAUSE = 0.2
-POST_GAME_PAUSE = 0.5
+TIME_PER_MOVE = 0.4
+MIN_MOVES = 50
+PRE_GAME_PAUSE = 0.5
+POST_GAME_PAUSE = 2
 SHUFFLE = True
 
-GECKODRIVER_PATH = '/home/matt/Projects/charkiver/geckodriver'
-AUDIO_SOURCE = 'alsa_output.usb-Razer_Razer_BlackShark_V2_Pro-00.analog-stereo.monitor'
-RECORDING_COORDS = [260,767]
-WINDOW_WIDTH = 728
-WINDOW_HEIGHT = 748
+X_0 = -30
+Y_0 = 490
+
+GECKODRIVER_PATH = '/home/matt/Projects/chess_archiver/geckodriver'
+service = Service(executable_path=GECKODRIVER_PATH)
+options = webdriver.FirefoxOptions()
+AUDIO_SOURCE = 'alsa_output.usb-Generic_USB_Audio-00.iec958-stereo.monitor'
+RECORDING_COORDS = [352,663]
+WINDOW_WIDTH = 856
+WINDOW_HEIGHT = 876
 
 def set_up_window(game):
-    driver = webdriver.Firefox(executable_path=GECKODRIVER_PATH)
+    driver = webdriver.Firefox(service=service, options=options)
 
     # open page and move to top left
     driver.get("https://www.chess.com/analysis?tab=analysis")
-    driver.set_window_position(0,0)
-
+    driver.set_window_position(X_0,Y_0)
+    driver.maximize_window()
     # copy PGN data
     pgn_text = game.accept(chess.pgn.StringExporter())
     pyperclip.copy(pgn_text)
@@ -87,7 +93,7 @@ def start_recording_process(file_name):
         '-f','pulse','-ac','2','-i',AUDIO_SOURCE,'-vcodec','libx264', \
         '-crf', '0','-x264-params','keyint=1',file_name]
 
-    # print(" ".join(cmd))
+    #print(" ".join(cmd))
     return subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
@@ -98,30 +104,76 @@ def play_game(driver, game):
         actions.send_keys(Keys.ARROW_RIGHT)
         actions.perform()
 
-def read_pgn_file(path, counter):
+def read_pgn_file(file_path, counter, limit):
     counter[0] += 1
     print(f"Reading PGN files - [{counter[0]}/{counter[1]}]", end='\r')
     games = []
-    with open(path) as file:
-        while True:
-            game = chess.pgn.read_game(file)
-            if game is None:
-                break  
-            games.append(game)
+
+    file_size = os.path.getsize(file_path)
+    print(f"file size: {file_size} bytes")
+
+
+    if file_size > 1000000:
+        print("opening file in pieces")
+        count = 0
+        with open(file_path) as pgn_file:
+            current_game = ""
+            for line in pgn_file:
+                current_game += line
+
+                # Check if the line marks the end of a game
+                if line.strip() == "1-0" or line.strip() == "0-1" or line.strip() == "1/2-1/2" or line.strip() == "*":
+
+                    count += 1
+                    if count % 100 == 0:
+                        print(f"read count: {count}, found {len(games)} matches", end='\r')
+
+                    game = chess.pgn.read_game(io.StringIO(current_game))
+                    if filter_game(game):
+                        counter[2] += 1
+                        games.append( game )
+                        if counter[2] >= limit:
+                            print(f"read count: {count}, found {len(games)} matches", end='\r')
+                            break
+                    current_game = ""
+            print("\nDone reading.")
+    else:
+        with open(file_path) as file:
+            while True:
+                game = chess.pgn.read_game(file)
+                counter[2] += 1 
+                if game is None:
+                    break  
+                games.append(game)
     return games
 
-def read_pgn_files(path, counter):
+def read_pgn_files(path, counter, limit):
     games = []
     if os.path.isfile(path):
-        games = np.concatenate((games, read_pgn_file(path, counter)))
+        games = np.concatenate((games, read_pgn_file(path, counter, limit)))
     elif os.path.isdir(path):
         for filename in os.listdir(path):
             sub_path = os.path.join(path, filename)
-            games = np.concatenate(( games, read_pgn_files(sub_path, counter)))
+            games = np.concatenate(( games, read_pgn_files(sub_path, counter, limit)))
     return games
 
 def filter_game(game):
+    info = extract_game_info(game)
 
+    # must have player
+    player = "capablanca"
+    white = info['white']
+    black = info['black']
+
+    if not ( player in black.lower() or player in white.lower() ):
+        return False
+    
+    # if not ( 'ob' in black.lower() or 'ob' in white.lower() ):
+    #     return False
+
+    # must have precise date
+    # if len(info['date']) < 5:
+    #     return False
     # min moves
     move_count = 0
     for move in game.mainline():
@@ -130,9 +182,9 @@ def filter_game(game):
         return False
 
     # only victories
-    result = game.headers["Result"]
-    if result != "1-0":
-        return False
+    # result = game.headers["Result"]
+    # if result != "1-0":
+    #     return False
 
     # only time stamped games
     # for move in game.mainline():
@@ -152,8 +204,15 @@ def add_overlay(index, video_path, info):
     font = ImageFont.truetype("./assets/cour_bold.ttf", 14)
     text_color = (255, 255, 255)  # White
     player_text_position = (20, WINDOW_HEIGHT - 15)
-    player_text = f'W - {info["white"]} ({info["white_elo"]})   vs   B - {info["black"]} ({info["black_elo"]})'
-    date_text_position = (WINDOW_WIDTH - 90, WINDOW_HEIGHT - 16)
+    player_text = f'W - {info["white"]}'
+    if info["white_elo"] != "":
+        player_text += f'({info["white_elo"]})'
+    player_text += "  vs  " 
+    player_text += f'B - {info["black"]}'
+    if info["black_elo"]:
+        player_text += f' ({info["black_elo"]})'
+        
+    date_text_position = (WINDOW_WIDTH - (len(info["date"]) * 10 ), WINDOW_HEIGHT - 16)
     draw.text(player_text_position, player_text, fill=text_color, font=font)
     draw.text(date_text_position, info["date"], fill=text_color, font=font )
 
@@ -172,15 +231,22 @@ def extract_game_info(game):
     info = {}
     info["white"] = game.headers["White"]
     info["black"] = game.headers["Black"]
-    info["white_elo"] = game.headers["WhiteElo"]
-    info["black_elo"] = game.headers["BlackElo"]
+    try:
+        info["white_elo"] = game.headers["WhiteElo"]
+        info["black_elo"] = game.headers["BlackElo"]
+    except:
+        info["white_elo"] = ""
+        info["black_elo"] = ""
 
     date = game.headers["Date"]
     if '??.??' in date:
         info["date"] = date[0:date.find(".")]
     else:
-        date_obj = datetime.strptime(date, '%Y.%m.%d')
-        info["date"] = date_obj.strftime('%m/%d/%Y')
+        try:
+            date_obj = datetime.strptime(date, '%Y.%m.%d')
+            info["date"] = date_obj.strftime('%m/%d/%Y')
+        except ValueError:
+            info["date"] = date.replace(".","/")
 
     # info["time_control"] = game.headers["TimeControl"]
     return info
@@ -204,17 +270,18 @@ async def main():
     else:
         limit = 1
     
-    counter = [0,0]
+    # current pgn file count, pgn file limit, current game count
+    counter = [0,0,0]
     if not os.path.isdir(input_path):
         counter[1] = 1
     else:
         for root, dirs, files in os.walk(input_path):
             counter[1] += len(files)
-    games = read_pgn_files( input_path, counter )
+    games = read_pgn_files( input_path, counter, limit )
     print(f"Reading PGN files - [{counter[1]}/{counter[1]}]")
-    print("Total Games: ", len(games))
+    #print("Total Games: ", len(games))
     
-    games = filter_games(games)
+    #games = filter_games(games)
     print("Filtered Games: ", len(games))
 
     if SHUFFLE:
